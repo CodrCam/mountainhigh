79071e6bc150e52859cf2f341a617c5a319ecba7
Cameron Griffin (He/Him) <ct.griffin7@gmail.com>
2023-09-17 14:24:40 -0700

    Added content into climbing page

--- src/pages/ClimbingPhotos.js
+++ src/pages/ClimbingPhotos.js
@@ -1,27 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../CSS/ClimbingPhotos.css';
+import { climbingPhotos } from './photosData';
 
 const ClimbingPhotos = () => {
+    const [selectedPhoto, setSelectedPhoto] = useState(null);
+
+    const handlePhotoClick = (id) => {
+        setSelectedPhoto(id);
+    };
+
+    const handleCloseModal = () => {
+        setSelectedPhoto(null);
+    };
+
     return (
         <div className="climbing-container">
-            <section className="parallax-section-climbing" id="climbing1">
-                <div className="content-container-climbing">
-                    <h1>Photo Title 1</h1>
-                    <p>Description about this specific climbing photo...</p>
+            {climbingPhotos.map((photo, i) => (
+                <React.Fragment key={photo.id}>
+                    <section className={`parallax-section-climbing`} id={photo.id}>
+                        <div className={`content-container-climbing ${i % 2 === 0 ? '' : 'right'}`}>
+                            <h1>{photo.title}</h1>
+                        </div>
+                        <div className={`thumbnail-container ${i % 2 === 0 ? 'right' : ''}`}>
+                            <img className="static-thumbnail" src={photo.thumbnail} alt={photo.title} onClick={() => handlePhotoClick(photo.id)} />
+                        </div>
+                    </section>
+                    <section className="white-space-climbing"></section>
+                </React.Fragment>
+            ))}
+
+            {selectedPhoto && (
+                <div className="photo-modal" onClick={handleCloseModal}>
+                    <div className="modal-content">
+                    <img src={climbingPhotos.find(p => p.id === selectedPhoto).thumbnail} alt={selectedPhoto} />
+                        <p>{climbingPhotos.find(p => p.id === selectedPhoto).description}</p>
+                    </div>
                 </div>
-            </section>
-            <section className="white-space-climbing"></section>
-
-            <section className="parallax-section-climbing" id="climbing2">
-                <div className="content-container-climbing right">
-                    <h1>Photo Title 2</h1>
-                    <p>Description about this specific climbing photo...</p>
-                </div>
-            </section>
-            <section className="white-space-climbing"></section>
-
-            {/* ... Add more sections as needed ... */}
-
+            )}
         </div>
     );
 }
---
+++ src/pages/photosData.js
@@ -0,0 +1,320 @@
+// photosData.js
+
+export const climbingPhotos = [
+  {
+    id: 'climbing1',
+    title: 'El Dorado',
+    description: "I stand atop Eldorado Mountain, the vastness of the North Cascades stretching around me. But all I see is an unending blanket of white. The whiteout conditions have blurred the line between land and sky, creating an almost surreal, dreamlike landscape. Beneath my boots, the snow merges seamlessly with the clouds. I'm tethered securely to my rope team, feeling the weight and pull of the line connecting us. Each breath I take crystallizes in the cold air, and despite the challenging environment, there's a sense of unity and determination among us as we navigate this ethereal world together.",
+    thumbnail: '/Media/Photos/Climbing/Climbing1.jpg'
+  },
+  {
+    id: 'climbing2',
+    title: 'Index Walls',
+    description: "I watch intently as Sam, our seasoned rock guide, expertly hammers in a bolt into the solid rock face. This bolt is essential, as it will anchor our portaledge where we plan to rest for the evening. The setting sun casts a golden hue over the vast expanse of the mountainous backdrop, emphasizing the magnitude of our adventurous endeavor. As the sound of the hammer reverberates through the quietness, there's a sense of anticipation and camaraderie between us. This isn't just about mastering big wall skills; it's also about trust, reliance, and the shared pursuit of a challenge.",
+    thumbnail: '/Media/Photos/Climbing/Climbing2.jpg'
+  },
+  {
+    id: 'climbing3',
+    title: 'Blues Buttress',
+    description: "On this ascent of Blues Buttress, I wedge my hand securely while leading one of the most rewarding pitches of this multi-pitch journey.",
+    thumbnail: '/Media/Photos/Climbing/Climbing3.jpg'
+  },
+  {
+    id: 'climbing4',
+    title: 'Vesper Peak',
+    description: "In this image, we're scaling Prusik Peak. During our climb on the second pitch, my partner's decisions raised concerns. Consequently, I took the lead, guiding us into a rappel descent to avoid the approaching night and prevent the circumstances from deteriorating further.",
+    thumbnail: '/Media/Photos/Climbing/Climbing4.jpg'
+  },
+  {
+    id: 'climbing5',
+    title: 'Washington Pass',
+    description: 'In this photo, a climber is seen navigating the final pitch of the renowned Liberty Bell route at Washington Pass.',
+    thumbnail: '/Media/Photos/Climbing/Climbing5.jpg'
+  },
+  {
+    id: 'climbing6',
+    title: 'Cutthroat Peak',
+    description: 'In this image, I was greeted by a stunning sunrise, mirroring the breathtaking sunset we witnessed the previous evening. We had chosen to rest mid-route on Cutthroat Peak in the North Cascades, embracing the adventure by climbing with our larger packs.',
+    thumbnail: '/Media/Photos/Climbing/Climbing6.jpg'
+  },
+  {
+    id: 'climbing7',
+    title: 'Cutthroat Approach',
+    description: "This image captures our approach to Cutthroat Peak, set against a scenic alpine meadow. Regrettably, just before this moment, I accidentally trod on a Yellow Jackets' nest, offering a surprising lesson on just how concealed their homes can be!",
+    thumbnail: '/Media/Photos/Climbing/Climbing7.jpg'
+  },
+  {
+    id: 'climbing8',
+    title: 'Technical Rope Rescue',
+    description: 'This photo was captured during an Alpine Institute Technical Rope Rescue course. Here, Shay is positioned up high, preparing an anchor to ensure we can safely transport a patient across challenging terrain.',
+    thumbnail: '/Media/Photos/Climbing/Climbing8.jpg'
+  },
+  {
+    id: 'climbing9',
+    title: 'Mt. Kulshan (Baker)',
+    description: "In this image, a 'patient' rescued by our team is visible. It was captured at the base of the Easton Glacier, where the ice fractures extensively, posing significant risks to those unfamiliar with glacier navigation.",
+    thumbnail: '/Media/Photos/Climbing/Climbing9.jpg'
+  },
+  {
+    id: 'climbing10',
+    title: 'North Bend',
+    description: "In this shot, Chris is rappelling amidst the North Bend wilderness, aiming to explore further climbing spots around the Exit 38 climbing area. It's soaking wet, which is just what we anticipated, giving us the perfect conditions to hone our dry tooling skills in damp environments.",
+    thumbnail: '/Media/Photos/Climbing/Climbing10.jpg'
+  },
+  {
+    id: 'climbing11',
+    title: 'Snow and Cliffs',
+    description: "Despite the snowfall, the urge to climb remains undeterred. In this image, Chris and I are defying the chilly conditions at North Bend's Exit 38 during fall, a testament to our determination not to let weather halt our climbing adventures.",
+    thumbnail: '/Media/Photos/Climbing/Climbing11.jpg'
+  },
+  {
+    id: 'climbing12',
+    title: 'Mt. Shuksan',
+    description: "In this frame, Mike is skillfully employing a snow anchor above to rappel over the edge. We're in practice mode, ensuring that when the situation demands, we can rely on these techniques confidently, especially in challenging circumstances.",
+    thumbnail: '/Media/Photos/Climbing/Climbing12.jpg'
+  },
+  {
+    id: 'climbing13',
+    title: 'The Cascades',
+    description: "While journeying through the Cascade Mountain Range, it's easy to overlook the origin of its name. The myriad of waterfalls cascading down the mountainsides serve as a constant, breathtaking reminder.",
+    thumbnail: '/Media/Photos/Climbing/Climbing13.jpg'
+  },
+  {
+    id: 'climbing14',
+    title: 'Smith Rock',
+    description: "In this shot, Sam is trailing on her inaugural multi-pitch at Smith Rock. We managed to start our climb early, beating the heat, and soon she'll be executing her very first rappel.",
+    thumbnail: '/Media/Photos/Climbing/Climbing14.jpg'
+  },
+  {
+    id: 'climbing15',
+    title: 'Mt. Erie',
+    description: 'This photo captures Sam as she tackles a challenging route on Mt. Erie, located near Bellingham, Washington.',
+    thumbnail: '/Media/Photos/Climbing/Climbing15.jpg'
+  },
+  {
+    id: 'climbing16',
+    title: 'The Boston Basin',
+    description: 'Description about climbing photo 1...',
+    thumbnail: '/Media/Photos/Climbing/Climbing16.jpg'
+  },
+  {
+    id: 'climbing17',
+    title: 'Boston Basin Washout',
+    description: 'During our climb of Forbidden Peak, we endured a rainstorm overnight, but by morning, the skies had cleared, revealing a beautiful day. However, when we returned to the car, we discovered the road had been washed out, casting doubt on our ability to drive our vehicle out. Thankfully, the forestry service came to our rescue, employing their tractors to clear the way efficiently.',
+    thumbnail: '/Media/Photos/Climbing/Climbing17.jpg'
+  },
+  {
+    id: 'climbing18',
+    title: 'Witch Doctor Wall',
+    description: "In this image, the vista from the Witch Doctor Wall near Darrington, Washington unfolds before us. We ventured along a route that, to our knowledge, hasn't been tackled since the 1990s. Our ascent neared the summit, but due to the imminent threat of rockfall, we had no choice but to make a strategic retreat.",
+    thumbnail: '/Media/Photos/Climbing/Climbing18.jpg'
+  },
+  {
+    id: 'climbing19',
+    title: 'Alpine Otters',
+    description: "Mountain otters? That's fascinating! While otters are generally associated with aquatic environments, nature has a way of surprising us. When rangers have verified their presence in mountainous regions, it's yet another testament to the adaptability and diversity of wildlife.",
+    thumbnail: '/Media/Photos/Climbing/Climbing19.jpg'
+  },
+  {
+    id: 'climbing20',
+    title: 'Sleeping Above the Clouds',
+    description: "In this frame, you can see James catching some rest on a petite boulder atop Vesper Peak, located near Darrington, Washington. Intriguingly, the backdrop showcases an atmospheric inversion, causing the clouds to settle within the valley below. Consequently, we're perched above this sea of clouds, basking under the brilliant azure skies.",
+    thumbnail: '/Media/Photos/Climbing/Climbing20.jpg'
+  },
+];
+
+export const skiingPhotos = [
+  {
+    id: 'skiing1',
+    title: 'Skiing Photo Title 1',
+    description: 'Description about skiing photo 1...',
+    thumbnail: '/Media/Photos/Skiing/Climbing0.jpg'
+  },
+  {
+    id: 'skiing2',
+    title: 'Skiing Photo Title 1',
+    description: 'Description about skiing photo 1...',
+    thumbnail: '/Media/Photos/Skiing/Climbing0.jpg'
+  },
+  {
+    id: 'skiing3',
+    title: 'Skiing Photo Title 1',
+    description: 'Description about skiing photo 1...',
+    thumbnail: '/Media/Photos/Skiing/Climbing0.jpg'
+  },
+  {
+    id: 'skiing4',
+    title: 'Skiing Photo Title 1',
+    description: 'Description about skiing photo 1...',
+    thumbnail: '/Media/Photos/Skiing/Climbing0.jpg'
+  },
+  {
+    id: 'skiing5',
+    title: 'Skiing Photo Title 1',
+    description: 'Description about skiing photo 1...',
+    thumbnail: '/Media/Photos/Skiing/Climbing0.jpg'
+  },
+  {
+    id: 'skiing6',
+    title: 'Skiing Photo Title 1',
+    description: 'Description about skiing photo 1...',
+    thumbnail: '/Media/Photos/Skiing/Climbing0.jpg'
+  },
+  {
+    id: 'skiing7',
+    title: 'Skiing Photo Title 1',
+    description: 'Description about skiing photo 1...',
+    thumbnail: '/Media/Photos/Skiing/Climbing0.jpg'
+  },
+  {
+    id: 'skiing8',
+    title: 'Skiing Photo Title 1',
+    description: 'Description about skiing photo 1...',
+    thumbnail: '/Media/Photos/Skiing/Climbing0.jpg'
+  },
+  {
+    id: 'skiing9',
+    title: 'Skiing Photo Title 1',
+    description: 'Description about skiing photo 1...',
+    thumbnail: '/Media/Photos/Skiing/Climbing0.jpg'
+  },
+  {
+    id: 'skiing10',
+    title: 'Skiing Photo Title 1',
+    description: 'Description about skiing photo 1...',
+    thumbnail: '/Media/Photos/Skiing/Climbing0.jpg'
+  },
+  {
+    id: 'skiing11',
+    title: 'Skiing Photo Title 1',
+    description: 'Description about skiing photo 1...',
+    thumbnail: '/Media/Photos/Skiing/Climbing0.jpg'
+  },
+  {
+    id: 'skiing12',
+    title: 'Skiing Photo Title 1',
+    description: 'Description about skiing photo 1...',
+    thumbnail: '/Media/Photos/Skiing/Climbing0.jpg'
+  },
+  {
+    id: 'skiing13',
+    title: 'Skiing Photo Title 1',
+    description: 'Description about skiing photo 1...',
+    thumbnail: '/Media/Photos/Skiing/Climbing0.jpg'
+  },
+  {
+    id: 'skiing14',
+    title: 'Skiing Photo Title 1',
+    description: 'Description about skiing photo 1...',
+    thumbnail: '/Media/Photos/Skiing/Climbing0.jpg'
+  },
+  {
+    id: 'skiing15',
+    title: 'Skiing Photo Title 1',
+    description: 'Description about skiing photo 1...',
+    thumbnail: '/Media/Photos/Skiing/Climbing0.jpg'
+  },
+  {
+    id: 'skiing16',
+    title: 'Skiing Photo Title 1',
+    description: 'Description about skiing photo 1...',
+    thumbnail: '/Media/Photos/Skiing/Climbing0.jpg'
+  },
+  {
+    id: 'skiing17',
+    title: 'Skiing Photo Title 1',
+    description: 'Description about skiing photo 1...',
+    thumbnail: '/Media/Photos/Skiing/Climbing0.jpg'
+  },
+  {
+    id: 'skiing18',
+    title: 'Skiing Photo Title 1',
+    description: 'Description about skiing photo 1...',
+    thumbnail: '/Media/Photos/Skiing/Climbing0.jpg'
+  },
+  {
+    id: 'skiing19',
+    title: 'Skiing Photo Title 1',
+    description: 'Description about skiing photo 1...',
+    thumbnail: '/Media/Photos/Skiing/Climbing0.jpg'
+  },
+  {
+    id: 'skiing20',
+    title: 'Skiing Photo Title 1',
+    description: 'Description about skiing photo 1...',
+    thumbnail: '/Media/Photos/Skiing/Climbing0.jpg'
+  },
+];
+
+export const urbanPhotos = [
+  {
+    id: 'urban1',
+    title: 'Urban Photo Title 1',
+    description: 'Description about urban photo 1...',
+    background: '/Media/Photos/Urban/Background1.jpg',
+    thumbnail: '/Media/Photos/Urban/Climbing0.jpg'
+  },
+  {
+    id: 'urban2',
+    title: 'Urban Photo Title 1',
+    description: 'Description about urban photo 1...',
+    background: '/Media/Photos/Urban/Background1.jpg',
+    thumbnail: '/Media/Photos/Urban/Climbing0.jpg'
+  },
+  {
+    id: 'urban3',
+    title: 'Urban Photo Title 1',
+    description: 'Description about urban photo 1...',
+    background: '/Media/Photos/Urban/Background1.jpg',
+    thumbnail: '/Media/Photos/Urban/Climbing0.jpg'
+  },
+  {
+    id: 'urban4',
+    title: 'Urban Photo Title 1',
+    description: 'Description about urban photo 1...',
+    background: '/Media/Photos/Urban/Background1.jpg',
+    thumbnail: '/Media/Photos/Urban/Climbing0.jpg'
+  },
+  {
+    id: 'urban5',
+    title: 'Urban Photo Title 1',
+    description: 'Description about urban photo 1...',
+    background: '/Media/Photos/Urban/Background1.jpg',
+    thumbnail: '/Media/Photos/Urban/Climbing0.jpg'
+  },
+  {
+    id: 'urban6',
+    title: 'Urban Photo Title 1',
+    description: 'Description about urban photo 1...',
+    background: '/Media/Photos/Urban/Background1.jpg',
+    thumbnail: '/Media/Photos/Urban/Climbing0.jpg'
+  },
+  {
+    id: 'urban7',
+    title: 'Urban Photo Title 1',
+    description: 'Description about urban photo 1...',
+    background: '/Media/Photos/Urban/Background1.jpg',
+    thumbnail: '/Media/Photos/Urban/Climbing0.jpg'
+  },
+  {
+    id: 'urban8',
+    title: 'Urban Photo Title 1',
+    description: 'Description about urban photo 1...',
+    background: '/Media/Photos/Urban/Background1.jpg',
+    thumbnail: '/Media/Photos/Urban/Climbing0.jpg'
+  },
+  {
+    id: 'urban9',
+    title: 'Urban Photo Title 1',
+    description: 'Description about urban photo 1...',
+    background: '/Media/Photos/Urban/Background1.jpg',
+    thumbnail: '/Media/Photos/Urban/Climbing0.jpg'
+  },
+  {
+    id: 'urban10',
+    title: 'Urban Photo Title 1',
+    description: 'Description about urban photo 1...',
+    background: '/Media/Photos/Urban/Background1.jpg',
+    thumbnail: '/Media/Photos/Urban/Climbing0.jpg'
+  },
+];
